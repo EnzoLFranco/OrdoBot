63d1bd7575bac1c654015f77ad1e4971d525bab2
Enzo Lemos Franco <fenzolemos@gmail.com>
2024-11-08 20:58:12 -0300

    Fix: Conserta o método de leitura de arquivos

--- Samubott Fake/src/main/java/LoadFile.java
+++ Samubott Fake/src/main/java/LoadFile.java
@@ -1,15 +1,13 @@
-import dev.langchain4j.data.document.Document;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 public class LoadFile {
-    public static Document loadDocument(String filePath) throws IOException {
+
+    public static String loadDocumentContent(String filePath) throws IOException {
         Path path = Path.of(filePath);
         List<String> lines = Files.readAllLines(path);
-        String documentContent = String.join("\n", lines);
-        return new Document(documentContent);
+        return String.join("\n", lines);
     }
 }
